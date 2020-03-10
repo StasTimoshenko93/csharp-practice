4905341470f95fc8121688fd55cedd26ae299d22
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-03-10 21:33:10 +0300

    add Task1

---
+++ Csharp.Practice/TASK1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TASK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short value = 5;
+            object valueO = value;
+            sbyte valueB = (sbyte)value;
+
+            Console.WriteLine($"Type :{valueB.GetType()}, Value {valueB} ");
+
+
+        }
+    }
+}
