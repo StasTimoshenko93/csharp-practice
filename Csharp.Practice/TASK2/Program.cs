f3ba73e6424d16482ec1e6c3eb20956b64cbc5fc
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-03-10 21:54:17 +0300

    added TASK2

---
+++ Csharp.Practice/TASK2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace TASK2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(45); 
+            arrayList.Add(true);
+            arrayList.Add("hello");
+            arrayList.Add(24.45);
+            foreach(object list in arrayList)
+            {
+                Console.WriteLine(list.GetType());
+            }
+        }
+    }
+}
