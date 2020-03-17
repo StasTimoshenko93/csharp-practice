9097b7c275723f2155166d8b7cf700d6fcbf4671
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-03-17 21:31:08 +0300

    done task1

---
+++ Csharp.Practice/lect.5Greeting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lect._5Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan ttimeNow = DateTime.Now.TimeOfDay;
+
+
+            int timeNow = ttimeNow.Hours;
+            //TimeSpan.FromHours
+            if (timeNow >= 9 && timeNow < 12)
+            {
+                Console.WriteLine("Good morning gays");
+            }
+            else if (timeNow >= 12 && timeNow < 15)
+            {
+                Console.WriteLine("Good day gays");
+            }
+            else if(timeNow >= 15 && timeNow < 22)
+            {
+                Console.WriteLine("Good evening gays");
+            }
+            
+        }
+    }
+}
