888b0eaa12957c6c4b6649c74afa7713c327f1aa
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-04-16 20:29:38 +0300

    added lect 12

---
+++ Csharp.Practice/Lect_12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lect_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnigueItem item = new UnigueItem();
+            Console.WriteLine(UnigueItem.Id);
+            UnigueItem item1 = new UnigueItem();
+            Console.WriteLine(UnigueItem.Id);
+            UnigueItem item2 = new UnigueItem();
+            Console.WriteLine(UnigueItem.Id);
+            UnigueItem item3 = new UnigueItem(5);
+            UnigueItem item4 = new UnigueItem(6);
+            Console.WriteLine(UnigueItem.Id);
+        }
+    }
+}
