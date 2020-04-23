238d2b6b516f46652bef9c332e4de6c1bedaf548
Stas Timoshenko <stastimoshenkogit@gmail.com>
2020-04-23 19:58:26 +0300

    added Task1

---
+++ Csharp.Practice/lect_14.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lect_14.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello, guys";
+            string y = s.StringExt();
+            Console.WriteLine(y);
+        }
+    }
+}
